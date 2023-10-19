71779fdf2148ff17045669692877a989c471200b
Enrique Román García Espino <70731354+ZorvShadow@users.noreply.github.com>
2023-10-19 01:24:32 +0000

    Ya cuenta espacios, comas y puntos

--- Algoritmia.java
+++ Algoritmia.java
@@ -37,6 +37,9 @@ public class Algoritmia {
         int numI = 0;
         int numO = 0;
         int numU = 0;
+        int numComas = 0;
+        int numPuntos = 0;
+        int numEspacios = 0;
 
         for (int i = 0; i < cadena.length; i++) {
             if (cadena[i] == 'a' || cadena[i] == 'A')
@@ -53,33 +56,56 @@ public class Algoritmia {
 
             if (cadena[i] == 'u' || cadena[i] == 'U')
                 numU++;
+
+            if (cadena[i] == ' ')
+                numEspacios++;
+
+            if (cadena[i] == '.')
+                numPuntos++;
+
+            if (cadena[i] == ',')
+                numComas++;
         }
 
-        char[] mensaje1 = {'A', ':', ' '};
+        char[] mensaje1 = { 'A', ':', ' ' };
         printChars(mensaje1);
         printChars(strToChar(numA + ""));
         System.out.println();
 
-        char[] mensaje2 = {'E', ':', ' '};
+        char[] mensaje2 = { 'E', ':', ' ' };
         printChars(mensaje2);
         printChars(strToChar(numE + ""));
         System.out.println();
 
-        char[] mensaje3 = {'I', ':', ' '};
+        char[] mensaje3 = { 'I', ':', ' ' };
         printChars(mensaje3);
         printChars(strToChar(numI + ""));
         System.out.println();
 
-        char[] mensaje4 = {'O', ':', ' '};
+        char[] mensaje4 = { 'O', ':', ' ' };
         printChars(mensaje4);
         printChars(strToChar(numO + ""));
         System.out.println();
 
-        char[] mensaje5 = {'U', ':', ' '};
+        char[] mensaje5 = { 'U', ':', ' ' };
         printChars(mensaje5);
         printChars(strToChar(numU + ""));
         System.out.println();
 
+        char[] mensaje6 = { 'E','s','p','a','c','i','o','s', ':', ' ' };
+        printChars(mensaje6);
+        printChars(strToChar(numEspacios + ""));
+        System.out.println();
+
+        char[] mensaje7 = { 'C','o','m','a','s', ':', ' ' };
+        printChars(mensaje7);
+        printChars(strToChar(numComas + ""));
+        System.out.println();
+
+        char[] mensaje8 = { 'P','u','n','t','o','s', ':', ' ' };
+        printChars(mensaje8);
+        printChars(strToChar(numPuntos + ""));
+        System.out.println();
 
         System.out.println();
     }
